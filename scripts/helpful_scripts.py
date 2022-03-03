89d89b576080b6b3323573156e751379baf3d5cd
mekhiya <mekhiya@gmail.com>
2022-03-03 22:04:21 +0530

    mainnet-dev-fork added

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,6 +1,8 @@
 from brownie import accounts, config, network, MockV3Aggregator
 from web3 import Web3
 
+FORKED_LOCAL_ENVIRONMENT = ["mainnet-fork", "mainnet-fork-dev"]
+
 DECIMALS = 8
 STARTING_PRICE = 200000000000
 
@@ -9,7 +11,10 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 def get_account():
     print(f"Active network is {network.show_active()}")
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENT
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
